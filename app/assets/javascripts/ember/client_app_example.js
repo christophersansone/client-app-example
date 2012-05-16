256df6cafdd675cb0c5e319b00cf9565d8c686e4
Christopher Sansone <christopher.sansone@iasta.com>
2012-05-16 11:49:38 -0400

    Added MVC namespaces

--- app/assets/javascripts/ember/client_app_example.js
+++ app/assets/javascripts/ember/client_app_example.js
@@ -1,11 +1,17 @@
-#= require_self
-#= require_tree ./models
-#= require_tree ./controllers
-#= require_tree ./views
-#= require_tree ./helpers
-#= require_tree ./templates
+//= require_self
+//= require_tree ./models
+//= require_tree ./controllers
+//= require_tree ./views
+//= require_tree ./helpers
+//= require_tree ./templates
 
 App = Ember.Application.create();
+
+App.Models = {};
+App.Controllers = {};
+App.Views = {};
+
+
 
 $(function() {
     App.layout = App.Layout.create();
