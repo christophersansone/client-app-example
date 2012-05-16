6ca44adac1da7af0554c54f2202e6f8383256e74
Christopher Sansone <christopher.sansone@iasta.com>
2012-05-16 13:12:19 -0400

    seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create! title: 'Avatar', year: 2009, mpaa_rating: 'PG-13'
+Movie.create! title: 'Titanic', year: 2009, mpaa_rating: 'PG-13'
+Movie.create! title: 'Harry Potter and the Deathly Hallows', year: 2011, mpaa_rating: 'PG-13'
+Movie.create! title: 'Transformers: Dark of the Moon', year: 2011, mpaa_rating: 'PG-13'
+Movie.create! title: 'The Lord of the Rings: The Return of the King', year: 2003, mpaa_rating: 'PG-13'
+Movie.create! title: 'Pirates of the Caribbean: Dead Man\'s Chest', year: 2006, mpaa_rating: 'PG-13'
+Movie.create! title: 'Toy Story 3', year: 2010, mpaa_rating: 'PG'
+Movie.create! title: 'Star Wars Episode 1', year: 1999, mpaa_rating: 'PG'
+Movie.create! title: 'Alice in Wonderland', year: 2010, mpaa_rating: 'PG-13'
+Movie.create! title: 'The Dark Knight', year: 2008, mpaa_rating: 'PG-13'
+
